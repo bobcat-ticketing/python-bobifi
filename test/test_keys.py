597fbf8ed61b047dd80b5cec25bb0f1ceb9e3c71
Jakob Schlyter <jakob@kirei.se>
2022-06-23 15:42:24 +0200

    reformat

--- test/test_keys.py
+++ test/test_keys.py
@@ -1,4 +1,5 @@
 import os
+
 from cryptojwt.jwx import key_from_jwk_dict
 
 from bobifi.samtrafiken import prod, test
@@ -7,7 +8,8 @@ from bobifi.samtrafiken import prod, test
 def test_jwks_test():
     _ = [key_from_jwk_dict(k) for k in test.trusted_jwks()["keys"]]
     os.stat(test.where())
-    
+
+
 def test_jwks_prod():
     _ = [key_from_jwk_dict(k) for k in test.trusted_jwks()["keys"]]
     os.stat(prod.where())
