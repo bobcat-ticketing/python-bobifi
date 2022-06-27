7292073f279731e32276cc4c59ff6ca853a363a6
Jakob Schlyter <jakob@kirei.se>
2022-06-27 08:24:40 +0200

    rename and improve

--- tests/test_keys.py
+++ tests/test_keys.py
@@ -1,7 +1,9 @@
 import os
 
+import pytest
 from cryptojwt.jwx import key_from_jwk_dict
-from bobifi.samtrafiken import where, trusted_jwks
+
+from bobifi.samtrafiken import trusted_jwks, where
 
 
 def test_jwks_test():
@@ -12,3 +14,8 @@ def test_jwks_test():
 def test_jwks_prod():
     _ = [key_from_jwk_dict(k) for k in trusted_jwks(env="prod")["keys"]]
     os.stat(where(env="prod"))
+
+
+def test_jwks_unknown():
+    with pytest.raises(KeyError):
+        _ = trusted_jwks(env="xyzzy")
--- tests/test_metadata.py
+++ tests/test_metadata.py
@@ -1,3 +1,4 @@
+import pytest
 import requests
 
 from bobifi.samtrafiken import metadata_url
@@ -6,5 +7,11 @@ from bobifi.samtrafiken import metadata_url
 def test_metadata_test():
     _ = requests.get(metadata_url(env="test"))
 
+
 def test_metadata_prod():
     _ = requests.get(metadata_url(env="prod"))
+
+
+def test_metadata_unknown():
+    with pytest.raises(KeyError):
+        _ = requests.get(metadata_url(env="xyzzy"))
