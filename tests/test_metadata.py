f28666e413d16e9e188f97c4103a98f961b64b7e
Jakob Schlyter <jakob@kirei.se>
2022-09-06 08:10:58 +0200

    allow query for both API v1 and v2

--- tests/test_metadata.py
+++ tests/test_metadata.py
@@ -5,11 +5,22 @@ from bobifi.samtrafiken import metadata_url
 
 
 def test_metadata_test():
-    _ = requests.get(metadata_url(env="test"))
+    _ = requests.get(metadata_url(env="test", version=1))
+    _ = requests.get(metadata_url(env="test", version=2))
 
 
 def test_metadata_prod():
-    _ = requests.get(metadata_url(env="prod"))
+    _ = requests.get(metadata_url(env="prod", version=1))
+    _ = requests.get(metadata_url(env="prod", version=2))
+
+
+def test_metadata_version():
+    url = metadata_url(env="prod")
+    assert "/v2/" in url
+    url = metadata_url(env="prod", version=1)
+    assert "/v1/" in url
+    url = metadata_url(env="prod", version=2)
+    assert "/v2/" in url
 
 
 def test_metadata_unknown():
