3e2a23db22b4bba46f997af06f815325d2fece5f
Jakob Schlyter <jakob@kirei.se>
2024-06-05 13:01:16 +0200

    add helper function to read JSON from file

--- bobifi/samtrafiken.py
+++ bobifi/samtrafiken.py
@@ -1,6 +1,5 @@
-import json
-
 from . import DATADIR
+from .utils import json_load_file
 
 DEFAULT_VERSION = 2
 
@@ -12,24 +11,24 @@ _ENVIRONMENTS = {
     1: {
         "test": {
             "keys_filename": JWKS_TEST_FILENAME,
-            "jwks": json.load(open(JWKS_TEST_FILENAME)),
+            "jwks": json_load_file(JWKS_TEST_FILENAME),
             "metadata_url": "https://bobmetadata-pp.samtrafiken.se/api/v1/participantMetadata",
         },
         "prod": {
             "keys_filename": JWKS_PROD_FILENAME,
-            "jwks": json.load(open(JWKS_PROD_FILENAME)),
+            "jwks": json_load_file(JWKS_PROD_FILENAME),
             "metadata_url": "https://bobmetadata.samtrafiken.se/api/v1/participantMetadata",
         },
     },
     2: {
         "test": {
             "keys_filename": JWKS_TEST_FILENAME,
-            "jwks": json.load(open(JWKS_TEST_FILENAME)),
+            "jwks": json_load_file(JWKS_TEST_FILENAME),
             "metadata_url": "https://bobmetadata-pp.samtrafiken.se/api/v2/participantMetadata",
         },
         "prod": {
             "keys_filename": JWKS_PROD_FILENAME,
-            "jwks": json.load(open(JWKS_PROD_FILENAME)),
+            "jwks": json_load_file(JWKS_PROD_FILENAME),
             "metadata_url": "https://bobmetadata.samtrafiken.se/api/v2/participantMetadata",
         },
     },
